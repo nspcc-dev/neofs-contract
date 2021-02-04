5d8c07b6e5a68ff0ba460252766fbe0e2a1a9774
Leonard Lyubich <leonard@nspcc.ru>
2021-02-04 10:22:54 +0300

    [#42] common: Replace IRNode type and InnerRingInvoker func to ir.go file
    Remove empty invoke.go source file.
    
    Signed-off-by: Leonard Lyubich <leonard@nspcc.ru>

--- common/ir.go
+++ common/ir.go
@@ -3,10 +3,27 @@ package common
 import (
 	"github.com/nspcc-dev/neo-go/pkg/interop"
 	"github.com/nspcc-dev/neo-go/pkg/interop/contract"
+	"github.com/nspcc-dev/neo-go/pkg/interop/runtime"
 	"github.com/nspcc-dev/neo-go/pkg/interop/storage"
 )
 
 const irListMethod = "innerRingList"
+
+type IRNode struct {
+	PublicKey []byte
+}
+
+// InnerRingInvoker returns public key of inner ring node that invoked contract.
+func InnerRingInvoker(ir []IRNode) []byte {
+	for i := 0; i < len(ir); i++ {
+		node := ir[i]
+		if runtime.CheckWitness(node.PublicKey) {
+			return node.PublicKey
+		}
+	}
+
+	return nil
+}
 
 // InnerRingList returns list of inner ring nodes through calling
 // "innerRingList" method of smart contract.
