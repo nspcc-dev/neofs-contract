d3dd2dedad73bff24c465d0147202515c4552eb7
Alex Vanin <alexey@nspcc.ru>
2021-07-13 21:08:27 +0300

    [#105] neofsid: Add docs
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- neofsid/doc.go
+++ neofsid/doc.go
@@ -1,2 +1,20 @@
-// NeoFSID contract description.
+/*
+NeoFSID contract is a contract deployed in NeoFS side chain.
+
+NeoFSID contract used to store connection between OwnerID and it's public keys.
+OwnerID is a 25-byte N3 wallet address that can be produced from public key.
+It is one-way conversion. In simple cases NeoFS verifies ownership by checking
+signature and relation between public key and OwnerID.
+
+In more complex cases, user can use public keys unrelated to OwnerID to maintain
+secure access to the data. NeoFSID contract stores relation between OwnerID and
+arbitrary public keys. Data owner can bind or unbind public key with it's account
+by invoking Bind or Unbind methods of NeoFS contract in main chain. After that,
+Alphabet nodes produce multi signed AddKey and RemoveKey invocations of NeoFSID
+contract.
+
+Contract notifications
+
+NeoFSID contract does not produce notifications to process.
+*/
 package neofsid
