3e7c2c3635b5603d34100ae61c3b05773c880309
Evgenii Stratonikov <evgeniy@nspcc.ru>
2021-10-26 14:28:14 +0300

    [#161] tests: reuse neo-go testing framework
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.14
 
 require (
 	github.com/mr-tron/base58 v1.2.0
-	github.com/nspcc-dev/neo-go v0.97.2
+	github.com/nspcc-dev/neo-go v0.97.4-pre.0.20211115125313-0e1fe2f58b1a
 	github.com/stretchr/testify v1.7.0
-	go.uber.org/zap v1.18.1
 )
--- tests/netmap_test.go
+++ tests/netmap_test.go
@@ -3,90 +3,88 @@ package tests
 import (
 	"math/big"
 	"math/rand"
+	"path"
 	"testing"
 
-	"github.com/nspcc-dev/neo-go/pkg/core"
 	"github.com/nspcc-dev/neo-go/pkg/encoding/bigint"
+	"github.com/nspcc-dev/neo-go/pkg/neotest"
 	"github.com/nspcc-dev/neo-go/pkg/util"
+	"github.com/nspcc-dev/neo-go/pkg/vm"
 	"github.com/nspcc-dev/neo-go/pkg/vm/stackitem"
-	"github.com/nspcc-dev/neo-go/pkg/wallet"
 	"github.com/nspcc-dev/neofs-contract/container"
 	"github.com/stretchr/testify/require"
 )
 
 const netmapPath = "../netmap"
 
-func deployNetmapContract(t *testing.T, bc *core.Blockchain, addrBalance, addrContainer util.Uint160, config ...interface{}) util.Uint160 {
+func deployNetmapContract(t *testing.T, e *neotest.Executor, addrBalance, addrContainer util.Uint160, config ...interface{}) util.Uint160 {
+	_, pubs, ok := vm.ParseMultiSigContract(e.Committee.Script())
+	require.True(t, ok)
+
 	args := make([]interface{}, 5)
 	args[0] = false
 	args[1] = addrBalance
 	args[2] = addrContainer
-	args[3] = []interface{}{CommitteeAcc.PrivateKey().PublicKey().Bytes()}
+	args[3] = []interface{}{pubs[0]}
 	args[4] = append([]interface{}{}, config...)
-	return DeployContract(t, bc, netmapPath, args)
+
+	c := neotest.CompileFile(t, e.CommitteeHash, netmapPath, path.Join(netmapPath, "config.yml"))
+	e.DeployContract(t, c, args)
+	return c.Hash
 }
 
-func prepareNetmapContract(t *testing.T, bc *core.Blockchain, config ...interface{}) util.Uint160 {
-	addrNNS := DeployContract(t, bc, nnsPath, nil)
+func newNetmapInvoker(t *testing.T, config ...interface{}) *neotest.ContractInvoker {
+	e := newExecutor(t)
 
-	ctrNetmap, err := ContractInfo(CommitteeAcc.Contract.ScriptHash(), netmapPath)
-	require.NoError(t, err)
+	ctrNNS := neotest.CompileFile(t, e.CommitteeHash, nnsPath, path.Join(nnsPath, "config.yml"))
+	ctrNetmap := neotest.CompileFile(t, e.CommitteeHash, netmapPath, path.Join(netmapPath, "config.yml"))
+	ctrBalance := neotest.CompileFile(t, e.CommitteeHash, balancePath, path.Join(balancePath, "config.yml"))
+	ctrContainer := neotest.CompileFile(t, e.CommitteeHash, containerPath, path.Join(containerPath, "config.yml"))
 
-	ctrBalance, err := ContractInfo(CommitteeAcc.Contract.ScriptHash(), balancePath)
-	require.NoError(t, err)
-
-	ctrContainer, err := ContractInfo(CommitteeAcc.Contract.ScriptHash(), containerPath)
-	require.NoError(t, err)
-
-	deployContainerContract(t, bc, ctrNetmap.Hash, ctrBalance.Hash, addrNNS)
-	deployBalanceContract(t, bc, ctrNetmap.Hash, ctrContainer.Hash)
-	return deployNetmapContract(t, bc, ctrBalance.Hash, ctrContainer.Hash, config...)
+	e.DeployContract(t, ctrNNS, nil)
+	deployContainerContract(t, e, ctrNetmap.Hash, ctrBalance.Hash, ctrNNS.Hash)
+	deployBalanceContract(t, e, ctrNetmap.Hash, ctrContainer.Hash)
+	deployNetmapContract(t, e, ctrBalance.Hash, ctrContainer.Hash, config...)
+	return e.CommitteeInvoker(ctrNetmap.Hash)
 }
 
 func TestDeploySetConfig(t *testing.T) {
-	bc := NewChain(t)
-	h := prepareNetmapContract(t, bc, "SomeKey", "TheValue", container.AliasFeeKey, int64(123))
-
-	tx := PrepareInvoke(t, bc, CommitteeAcc, h, "config", "SomeKey")
-	CheckTestInvoke(t, bc, tx, "TheValue")
-
-	tx = PrepareInvoke(t, bc, CommitteeAcc, h, "config", container.AliasFeeKey)
-	CheckTestInvoke(t, bc, tx, bigint.ToBytes(big.NewInt(123)))
+	c := newNetmapInvoker(t, "SomeKey", "TheValue", container.AliasFeeKey, int64(123))
+	c.Invoke(t, "TheValue", "config", "SomeKey")
+	c.Invoke(t, stackitem.NewByteArray(bigint.ToBytes(big.NewInt(123))),
+		"config", container.AliasFeeKey)
 }
 
-func dummyNodeInfo(acc *wallet.Account) []byte {
+func dummyNodeInfo(acc neotest.Signer) []byte {
 	ni := make([]byte, 66)
 	rand.Read(ni)
-	copy(ni[2:], acc.PrivateKey().PublicKey().Bytes())
+
+	pub, _ := vm.ParseSignatureContract(acc.Script())
+	copy(ni[2:], pub)
 	return ni
 }
 
 func TestAddPeer(t *testing.T) {
-	bc := NewChain(t)
-	h := prepareNetmapContract(t, bc)
+	c := newNetmapInvoker(t)
 
-	acc := NewAccount(t, bc)
+	acc := c.NewAccount(t)
+	cAcc := c.WithSigners(acc)
 	dummyInfo := dummyNodeInfo(acc)
 
-	acc1 := NewAccount(t, bc)
-	tx := PrepareInvoke(t, bc, acc1, h, "addPeer", dummyInfo)
-	AddBlock(t, bc, tx)
-	CheckFault(t, bc, tx.Hash(), "witness check failed")
+	acc1 := c.NewAccount(t)
+	cAcc1 := c.WithSigners(acc1)
+	cAcc1.InvokeFail(t, "witness check failed", "addPeer", dummyInfo)
 
-	tx = PrepareInvoke(t, bc, acc, h, "addPeer", dummyInfo)
-	AddBlock(t, bc, tx)
-
-	aer := CheckHalt(t, bc, tx.Hash())
+	h := cAcc.Invoke(t, stackitem.Null{}, "addPeer", dummyInfo)
+	aer := cAcc.CheckHalt(t, h)
 	require.Equal(t, 1, len(aer.Events))
 	require.Equal(t, "AddPeer", aer.Events[0].Name)
 	require.Equal(t, stackitem.NewArray([]stackitem.Item{stackitem.NewByteArray(dummyInfo)}),
 		aer.Events[0].Item)
 
 	dummyInfo[0] ^= 0xFF
-	tx = PrepareInvoke(t, bc, acc, h, "addPeer", dummyInfo)
-	AddBlock(t, bc, tx)
-
-	aer = CheckHalt(t, bc, tx.Hash())
+	h = cAcc.Invoke(t, stackitem.Null{}, "addPeer", dummyInfo)
+	aer = cAcc.CheckHalt(t, h)
 	require.Equal(t, 1, len(aer.Events))
 	require.Equal(t, "AddPeer", aer.Events[0].Name)
 	require.Equal(t, stackitem.NewArray([]stackitem.Item{stackitem.NewByteArray(dummyInfo)}),
