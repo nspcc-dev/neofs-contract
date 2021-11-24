1d3bfc5fa54448deb1c25a3830af7ed343cc9c58
Pavel Karpy <carpawell@nspcc.ru>
2021-11-24 20:26:54 +0300

    [#174] subnet: Add docs
    Signed-off-by: Pavel Karpy <carpawell@nspcc.ru>

---
+++ subnet/doc.go
@@ -0,0 +1,37 @@
+/*
+Subnet contract is a contract deployed in NeoFS side chain.
+
+Subnet contract stores and manages NeoFS subnetwork states. It allows registering
+and deleting subnetworks, limiting access to them and defining a list of the Storage
+Nodes that can be included in them.
+
+Contract notifications
+
+Put notification. This notification is produced when new subnetwork is
+registered by invoking Put method.
+
+  Put
+    - name: id
+      type: ByteArray
+    - name: ownerKey
+      type: PublicKey
+    - name: info
+      type: ByteArray
+
+Delete notification. This notification is produced when some subnetwork is
+deleted by invoking Delete method.
+
+  Delete
+    - name: id
+      type: ByteArray
+
+RemoveNode notification. This notification is produced when some node is deleted
+by invoking RemoveNode method.
+
+  RemoveNode
+    - name: subnetID
+      type: ByteArray
+    - name: node
+      type: PublicKey
+*/
+package subnet
