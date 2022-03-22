41b79077604f7f258efba6b4715f3808bbfaef9e
Evgenii Stratonikov <evgeniy@nspcc.ru>
2022-03-22 11:58:23 +0300

    [#228] common: Bump contract version
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- common/version.go
+++ common/version.go
@@ -4,15 +4,15 @@ import "github.com/nspcc-dev/neo-go/pkg/interop/native/std"
 
 const (
 	major = 0
-	minor = 14
-	patch = 2
+	minor = 15
+	patch = 0
 
 	// Versions from which an update should be performed.
 	// These should be used in a group (so prevMinor can be equal to minor if there are
 	// any migration routines.
 	prevMajor = 0
 	prevMinor = 14
-	prevPatch = 0
+	prevPatch = 2
 
 	Version = major*1_000_000 + minor*1_000 + patch
 
