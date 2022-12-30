c57a877e34573da8c37a525f633a720832644b30
Roman Khimov <roman@nspcc.ru>
2022-12-30 13:08:49 +0300

    common: bump version, do netmap migrations pre-0.16.0
    The next release will be at least 0.16.1, so it's safe to assume that the
    migration code in the netmap contract is only needed pre-0.16.0 now.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- common/version.go
+++ common/version.go
@@ -5,7 +5,7 @@ import "github.com/nspcc-dev/neo-go/pkg/interop/native/std"
 const (
 	major = 0
 	minor = 16
-	patch = 0
+	patch = 1
 
 	// Versions from which an update should be performed.
 	// These should be used in a group (so prevMinor can be equal to minor if there are
