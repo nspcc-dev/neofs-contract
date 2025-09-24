c4b9c29d466bb368c3fe41b59cb53804547918b1
Roman Khimov <roman@nspcc.ru>
2025-09-24 17:44:46 +0300

    contracts: use range over integers where appropriate
    Now available in NeoGo.
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- contracts/neofs/contract.go
+++ contracts/neofs/contract.go
@@ -101,7 +101,7 @@ func _deploy(data any, isUpdate bool) {
 		panic("bad configuration")
 	}
 
-	for i := 0; i < ln/2; i++ { //nolint:intrange // Not supported by NeoGo
+	for i := range ln / 2 {
 		key := args.config[i*2]
 		val := args.config[i*2+1]
 
